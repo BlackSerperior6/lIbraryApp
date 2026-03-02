c7989788fddaa2061c4536f6fd6df62e2af5373c
BlackSerperior6 <m330.nek76@gmail.com>
2026-03-02 20:45:49 +0500

    Еще мучения

---
+++ LibraryAppWeb/Pages/ControlPanel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibraryAppWeb.Pages
+{
+    public class ControlPanelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
