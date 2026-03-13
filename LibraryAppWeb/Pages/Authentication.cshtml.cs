b8fa2945ad1c8f7642d63fc1310212f927f594f4
BlackSerperior6 <m330.nek76@gmail.com>
2026-03-13 12:55:10 +0500

    ОНО РАБОТАЕТ

--- LibraryAppWeb/Pages/Authentication.cshtml.cs
+++ LibraryAppWeb/Pages/Authentication.cshtml.cs
@@ -19,17 +19,36 @@ namespace LibraryAppWeb.Pages
 
         public async Task<IActionResult> OnPostAsync()
         {
-            string query = $"SELECT \"PasswordHash\", \"PasswordSalt\", \"Role\" from \"DatabaseUsers\" WHERE 'Login' = @login;";
+            string query = $"SELECT \"PasswordHash\", \"Role\" from \"DatabaseUsers\" WHERE \"Login\"    = @login;";
 
             try
             {
+                /*var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, Login),
+                    new Claim(ClaimTypes.Role, "Admin")
+                };
+
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(new ClaimsIdentity(claims, "Cookies")));
+
+                var returnUrl= Request.Query["returnUrl"].ToString();
+
+                if (!string.IsNullOrWhiteSpace(returnUrl))
+                    return Redirect(returnUrl);
+                else
+                    return RedirectToPage("/ControlPanel");*/
+
                 await using var connection = DataBaseConnectionFactory.CreateConnection();
 
                 var user = await connection.QueryFirstOrDefaultAsync
-                    <(string passwordHash, string passwordSalt, string role)>(query, new { login = Login });
+                    <(string passwordHash, string role)>(query, new { login = Login });
 
-                if (!string.IsNullOrWhiteSpace(user.passwordSalt))
+                if (!string.IsNullOrWhiteSpace(user.passwordHash))
                 {
+                    bool isValid = BCrypt.Net.BCrypt.Verify(Password, user.passwordHash);
+
                     if (isValid)
                     {
                         var claims = new List<Claim>
@@ -53,7 +72,7 @@ namespace LibraryAppWeb.Pages
             }
             catch (Exception e)
             {
-                ErrorMessage = $"Произошла ошибка:\n{e}. Пожалуйста, обратитесь к администратору!";
+                ErrorMessage = $"Произошла ошибка:\n{e}.\nПожалуйста, обратитесь к администратору!";
             }
 
             ErrorMessage = "Неверный логин или пароль!";
